1abed8b6cc4b4b8d1ddfd9177eadd67b8b0dd0af
Rabina Shrestha <rabinaa.shresthaa@gmail.com>
2022-11-12 15:29:35 +0545

    update dp

--- app/src/main/java/com/example/hackathon/fragment_explorebox.kt
+++ app/src/main/java/com/example/hackathon/fragment_explorebox.kt
@@ -1,14 +1,11 @@
 package com.example.hackathon
 
-import android.app.DatePickerDialog
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import java.util.*
 
 class frament_explorebox : Fragment(),AdapterView.OnItemSelectedListener {
 
@@ -17,7 +14,7 @@ class frament_explorebox : Fragment(),AdapterView.OnItemSelectedListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.fragment_explorebox, container, false)
+        val v = inflater.inflate(R.layout.fragment_find_donor, container, false)
 
         val spinner_loc: Spinner = v.findViewById(R.id.spinnerLocation)
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -48,13 +45,13 @@ class frament_explorebox : Fragment(),AdapterView.OnItemSelectedListener {
         return v
     }
 
-    override fun onItemSelected(parent: AdapterView<>?, view: View?, pos: Int, id: Long) {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
         // val selection= parent?.getItemAtPosition(pos)
         val item= parent?.selectedItem
         //println(item)
         //Toast.makeText(context,"$item is selected", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onNothingSelected(p0: AdapterView<>?) {
+    override fun onNothingSelected(p0: AdapterView<*>?) {
         TODO("Not yet implemented")
     }}
